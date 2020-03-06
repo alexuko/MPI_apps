17450184550e4953724c02037afc929767090e6f
Alejandro <alextek1885@hotmail.com>
2020-03-06 00:45:20 +0000

    testing mpi computation application

--- mpi_tutorial_apps/app7_MPI_Barrier_to_force_synchronisation.cpp
+++ mpi_tutorial_apps/app7_MPI_Barrier_to_force_synchronisation.cpp
@@ -12,7 +12,7 @@ int main(int argc, char** argv){
 
     std::cout << "rank: " << world_rank << " world size: " << world_size << std::endl;
     srand(world_rank);
-    int sleep_time = rand() % 20;
+    int sleep_time = rand() % 10;
     std::cout << "Sleeping time: " << sleep_time <<  std::endl;
     sleep(sleep_time);    
     std::cout << "node " << world_rank << " exiting sleep and synchronising" << std::endl;
@@ -20,8 +20,10 @@ int main(int argc, char** argv){
     MPI_Barrier(MPI_COMM_WORLD);
     std::cout << "node " << world_rank << " synchronised" << std::endl;
     
-  
+
+    
 
     MPI_Finalize();
     return 0;
 }
+        
