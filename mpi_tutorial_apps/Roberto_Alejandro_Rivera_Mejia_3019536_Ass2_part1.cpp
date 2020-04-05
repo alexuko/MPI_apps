9ab256299eed24e3bc3beff0f2f185af691e0984
Alejandro <alextek1885@hotmail.com>
2020-04-05 12:17:47 +0100

    code optimized and commented

--- mpi_tutorial_apps/Roberto_Alejandro_Rivera_Mejia_3019536_Ass2_part1.cpp
+++ mpi_tutorial_apps/Roberto_Alejandro_Rivera_Mejia_3019536_Ass2_part1.cpp
@@ -13,10 +13,10 @@ int primeCounter(int n);
 //method that returns the result of how long each node takes to run the primeCounter()
 double performOperation();
 
-//method will perform the performOperation(), gather the result from the other nodes and print the results out. 
+//method will perform the performOperation(), gather the result from the other nodes and print the results out.
 void coordinator();
 
-//method will perform the performOperation() and send its result to the coordinator. 
+//method will perform the performOperation() and send its result to the coordinator.
 void participant();
 
 //main() will be executed by each node
@@ -56,11 +56,11 @@ void coordinator()
     totalResults = new double[world_size];
     //save the result of the operation
     result[0] = performOperation();
-    
+
     //gather the results from all the nodes including itself
     MPI_Gather(result, 1, MPI_DOUBLE, totalResults, 1, MPI_DOUBLE, root, MPI_COMM_WORLD);
-    
-    //printing results contained in array totalResults 
+
+    //printing results contained in array totalResults
     std::cout << "results: " << std::endl;
     for (unsigned int i = 0; i < world_size; i++)
     {
@@ -76,7 +76,7 @@ void participant()
 {
     //array of size one where we will store the node result
     result = new double[1];
-    //save the result of the operation    
+    //save the result of the operation
     result[0] = performOperation();
     // take part in the gather, sending its result to the root
     MPI_Gather(result, 1, MPI_DOUBLE, NULL, 1, MPI_DOUBLE, root, MPI_COMM_WORLD);
@@ -85,36 +85,34 @@ void participant()
 }
 
 double performOperation()
-{   
-    //variables to store initial time, ending time and the time it took to perform operation
-    double start, end, elapsed_time;
+{
+    //variables to store initial time and ending time.
+    double start, end;
     //save starting time
     start = MPI_Wtime();
     //perform the method primeCounter()
     primeCounter(pow(25, world_rank + 2));
-    //save the final time 
+    //save the final time
     end = MPI_Wtime();
-    //calculate the elapsed time 
-    elapsed_time = end - start;
-    //return the elapsed time it took to the rank to complete its operation 
-    return elapsed_time;
+    //return the elapsed time it took the rank to complete its operation
+    return end - start;
 }
 
 int primeCounter(int n)
-{   
+{
     //initialise counter to 0
     int count = 0;
     //the first of the prime numbers is 2, so we set our a variable = 2
     for (int a = 2; a < n; a++)
-    {   
-        //set the variable to true 
+    {
+        //set the variable to true
         bool isPrime = true;
         //and then procced with the check to find out if its a prime number
         for (int c = 2; c * c <= a; c++)
         {
             //if operation is equally divisible, then its not a prime number
             if (a % c == 0)
-            {                 
+            {
                 isPrime = false;
                 break;
             }
